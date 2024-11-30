cb4d24f9867318b7f0f12af482102a53327be1ec
GiorgioSV99 <118373082+GiorgioSV99@users.noreply.github.com>
2024-11-30 17:15:32 +0100

    Update baseModel.py

--- codes/centralRepo/baseModel.py
+++ codes/centralRepo/baseModel.py
@@ -43,6 +43,39 @@ import copy
 #sys.path.insert(1, os.path.join(masterPath, 'centralRepo'))
 import stopCriteria
 import samplers
+import torch
+import torch.nn as nn
+
+
+class PrototypeLoss(nn.Module):
+
+    def forward(self, features, proxy, labels):
+
+        label_prototypes = torch.index_select(proxy, dim=0, index=labels)
+
+        pl = huber_loss(features, label_prototypes, sigma=1)
+        pl_loss = torch.mean(pl)
+
+        return pl_loss
+
+
+def huber_loss(input, target, sigma=1):
+    beta = 1.0 / (sigma**2)
+    diff = torch.abs(input - target)
+    cond = diff < beta
+    loss = torch.where(cond, 0.5 * diff**2 / beta, diff - 0.5 * beta)
+
+    return torch.sum(loss, dim=1)
+
+class NormIncreaseLoss(nn.Module):
+    def __init__(self):
+        super(NormIncreaseLoss, self).__init__()
+
+    def forward(self, mat):
+
+        norms = torch.norm(mat, p=2, dim=1)
+        loss = -norms
+        return loss.mean()
 
 class baseModel():
     def __init__(
@@ -746,36 +779,3 @@ class baseModel():
         return out
 
 
-import torch
-import torch.nn as nn
-
-
-class PrototypeLoss(nn.Module):
-
-    def forward(self, features, proxy, labels):
-
-        label_prototypes = torch.index_select(proxy, dim=0, index=labels)
-
-        pl = huber_loss(features, label_prototypes, sigma=1)
-        pl_loss = torch.mean(pl)
-
-        return pl_loss
-
-
-def huber_loss(input, target, sigma=1):
-    beta = 1.0 / (sigma**2)
-    diff = torch.abs(input - target)
-    cond = diff < beta
-    loss = torch.where(cond, 0.5 * diff**2 / beta, diff - 0.5 * beta)
-
-    return torch.sum(loss, dim=1)
-
-class NormIncreaseLoss(nn.Module):
-    def __init__(self):
-        super(NormIncreaseLoss, self).__init__()
-
-    def forward(self, mat):
-
-        norms = torch.norm(mat, p=2, dim=1)
-        loss = -norms
-        return loss.mean()
