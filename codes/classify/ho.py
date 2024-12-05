1ba60c3859a5ffc935a64fa3470598858e7bede7
GiorgioSV99 <118373082+GiorgioSV99@users.noreply.github.com>
2024-12-05 13:00:32 +0100

    Update ho.py

--- codes/classify/ho.py
+++ codes/classify/ho.py
@@ -60,7 +60,11 @@ def ho(datasetId = None, network = None, nGPU = None, subTorun=None):
 
     #%% Set the defaults use these to quickly run the network
     datasetId = datasetId or 0
-    network = network or 'FBCNet'
+    !git clone https://github.com/GiorgioSV99/SST-DPN.git
+    %cd SST-DPN
+    from model import SST_DPN
+    network = SST_DPN(chans=22, samples=1000, num_classes=4, F1=9,
+                     F2=48, time_kernel1=75, pool_kernels=[50, 100, 200])
     nGPU = nGPU or 0
     subTorun= subTorun or None
     selectiveSubs = False
